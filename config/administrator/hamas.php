4a4877aec9df34a1cf1b04cca396a9aefe8f3b8a
Jakub <tortillaconalioli@gmail.com>
2016-01-05 17:13:44 +0100

    Initial commit

---
+++ config/administrator/hamas.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jakub
+ * Date: 05/01/2016
+ * Time: 0:04
+ */
+
+return [
+    'title' => 'Hamas',
+    'single' => 'hama',
+    'model' => 'App\Hama',
+    'columns' => [
+        'name' => [
+            'title' => 'Nombre',
+            'type' => 'text'
+        ]
+    ],
+    'edit_fields' => [
+        'name' => [
+            'title' => 'Nombre',
+            'type' => 'text'
+        ]
+    ]
+];
